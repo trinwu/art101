c18c779c7143b4e4207fa6ff280dffde677769d5
trinwu <trini.me28@gmail.com>
2024-10-16 03:18:04 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - an implementation of the game rock-paper-scissors
+// Author: Trinity Wu <trawu@ucsc.edu>
+// Date: October 17, 2024
+
+// Set Up the Game Environment
+  // Provide options for the player to choose between "Rock," "Paper," or "Scissors." 
+// Handle Player Input
+  // Wait for the player to select one of the options (Rock, Paper, or Scissors). 
+  // Validate the player's input. 
+// Generate the Computer's Choice
+// Compare Player and Computer Choices
+// Display the Result
+  // Show the player's choice and the computer's choice. 
+  // Announce the outcome (Win, Loss, or Tie). 
+// Ask the Player if They Want to Play Again
+// 7. Handle End of Game
+
 
 // Constants
 
