4da33fc0650085c79953e6519157744e24d76759
trinwu <trini.me28@gmail.com>
2024-10-21 01:43:28 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Trinity Wu <trawu@ucsc.edu>
+// Date: October 21, 2024
 
-// Constants
+// Define variables
+make = "Lexus";
+model = "RX";
+color = "Cloudburst Gray";
+year = 2024;
 
-// Functions
+// Calculate age
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
