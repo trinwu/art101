622e4b01d24969a30b5ca54994c5f045d50d1e07
trinwu <trini.me28@gmail.com>
2024-11-04 15:42:19 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -7,39 +7,11 @@
    Date: November 5, 2024
 */
 
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+// Add the "Make Special" button to each section with the "special-section" class
+$(".minor-section").append("<button class='make-special'>Make Special</button>");
 
-// add a click listener to the challenge button
-$("#button-challenge").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+// Add a click event listener to all buttons with the "make-special" class
+$(".minor-section .make-special").click(function() {
+    // Toggle the "special" class on the parent of the button clicked
+    $(this).parent().toggleClass("special");
 });
-
-// add button to problems section
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-
-// add a click listener to the problems button
-$("#button-problems").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
-});
-
-// add button to reflection section
-$("#reflection").append("<button id='button-reflection'>Make Special</button>");
-
-// add a click listener to the reflection button
-$("#button-reflection").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#reflection").toggleClass("special");
-});
-
-// add button to results section
-$("#results").append("<button id='button-results'>Make Special</button>");
-
-// add a click listener to the results button
-$("#button-results").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#results").toggleClass("special");
-});
-
